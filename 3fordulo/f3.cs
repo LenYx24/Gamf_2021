45b96e8a63b5bbe805518b36b47c4bdcb5f756e7
RedBird <35497481+redbirdjs@users.noreply.github.com>
2021-11-26 19:04:19 +0100

    3. feladat kész

--- 3fordulo/f3.cs
+++ 3fordulo/f3.cs
@@ -23,35 +23,81 @@ namespace _3f
         static List<SzamHarmas> harmasok = new List<SzamHarmas>();
         static bool debug = false;
         public static void output(){
-
-            /*
-            
-                Első feladat: Pitagoraszi számhármasok száma, ha a = 15 és b legfejebb = 1000000;
-                Második feladat: 
-            
-            */
-
             Console.WriteLine("--------------------");
-            ulong a = 15;
-            for (ulong i = 1; i < 1000000; i++) {
-                ulong cnegyzet = a*a+i*i;
+            feladatA(15, true);
+            feladatB();
+            feladatC();
+        }
+
+        /*    
+            Első feladat: Pitagoraszi számhármasok száma, ha a = 15 és b legfejebb = 1000000;
+        */
+
+        static void feladatA(ulong a, bool giveOutput) {
+            for (ulong b = 1; b < 1000000; b++) {
+                ulong cnegyzet = a*a+b*b;
                 if (debug) Console.WriteLine("Sqr:" + cnegyzet.ToString());
 
                 double c = Math.Sqrt(cnegyzet);
                 if (debug) Console.WriteLine("Sqrt: " + c.ToString());
 
                 ulong d;
-                if (debug) Console.WriteLine(a.ToString() + "^2 * " + i.ToString() + "^2 = " + cnegyzet + "\n----");
+                if (debug) Console.WriteLine(a.ToString() + "^2 * " + b.ToString() + "^2 = " + cnegyzet + "\n----");
 
                 if (ulong.TryParse(c.ToString(), out d)) {
-                    harmasok.Add(new SzamHarmas(a, i, d));
+                    harmasok.Add(new SzamHarmas(a, b, d));
                 }
             }
-            Console.WriteLine("A szám".PadRight(20) + "B szám".PadRight(20) + "C szám".PadRight(20));
-            for (int i = 0; i < harmasok.Count; i++) {
-                Console.WriteLine(harmasok[i].a.ToString().PadRight(20) + harmasok[i].b.ToString().PadRight(20) + harmasok[i].c.ToString().PadRight(20));
+            if (giveOutput) Console.WriteLine("-----------------\nA szám".PadRight(20) + "B szám".PadRight(20) + "C szám".PadRight(20));
+            for (int b = 0; b < harmasok.Count; b++) {
+                if (giveOutput) Console.WriteLine(harmasok[b].a.ToString().PadRight(20) + harmasok[b].b.ToString().PadRight(20) + harmasok[b].c.ToString().PadRight(20));
             }
-            Console.WriteLine("Pitagoraszi számhármasok: ".PadRight(20) + harmasok.Count);
+            if (giveOutput) Console.WriteLine("-----------------\nPitagoraszi számhármasok: ".PadRight(20) + harmasok.Count);
+        }
+
+        /*
+            Második feladat: Az a feladatban 
+        */
+
+        static void feladatB() {
+            int legnagyobb = 0;
+            for (int i = 0; i < harmasok.Count; i++) {
+                ulong szorzat = 0;
+                if (i < harmasok.Count) {
+                    szorzat = harmasok[i].a * harmasok[i].b * harmasok[i].c;
+                }
+                if (debug) Console.WriteLine((i+1) + ". szorzat: " + szorzat + "(" + harmasok[i].a + " * " + harmasok[i].b + " * " + harmasok[i].c + ")");
+                if (legnagyobb < (int)szorzat) {
+                    legnagyobb = (int)szorzat;
+                }
+            }
+            Console.WriteLine("-----------------\nLegnagyobb számhármas szozata: " + legnagyobb);
+        }
+
+        static void feladatC() {
+            int counter = 0;
+            List<ulong> aSzamok = new List<ulong>();
+            List<ulong> bSzamok = new List<ulong>();
+            List<ulong> cSzamok = new List<ulong>();
+            for (ulong a = 1; a < 2401; a++) {
+                for (ulong b = 1; b < 2401; b++) {
+                    ulong negyzet = a*a + b*b;
+
+                    double c = Math.Sqrt(negyzet);
+
+                    ulong d;
+                    if (ulong.TryParse(c.ToString(), out d)) {
+                        if (a+b+c == 2400 && !aSzamok.Contains(a) && !bSzamok.Contains(b) && !cSzamok.Contains(d)) {
+                            if (debug) Console.WriteLine(a.ToString().PadRight(20) + b.ToString().PadRight(20) + c.ToString().PadRight(20) + (a+b+c).ToString());
+                            counter++;
+                            aSzamok.Add(a);
+                            bSzamok.Add(b);
+                            cSzamok.Add(d);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("-----------------\nSzámhármasok: " + counter);
         }
     }
 }
